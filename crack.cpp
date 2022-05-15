14ea411ebf377f62f22556e7e064f5b80b585199
tashviks <105586514+tashviks@users.noreply.github.com>
2022-05-15 15:49:13 +0530

    Add files via upload

---
+++ crack.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n ;
+	cin>>n;
+
+	while(n--)
+	{
+		int n ;
+		std::vector<int> v;
+		pair<int , int >p;
+
+		std::vector<char> v;
+		if (FILE *fp = fopen("filename", "r"))
+		{
+			char buf[1024];
+			while (size_t len = fread(buf, 1, sizeof(buf), fp))
+				v.insert(v.end(), buf, buf + len);
+			fclose(fp);
+		}
+	}
+		for (int i = 0; i < count; ++i)
+		{
+			int n ;
+			cin>>n;
+			
+			int tas=0;
+			while(tas++)
+			{
+				if(tas%n==0);val
+				{
+					if(val == 675)
+						break;
+				}
+
+			}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+}
